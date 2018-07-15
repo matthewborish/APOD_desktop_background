2341859439c79de488a136989aae3a327b564839
matthew.borish@gmail.com <matthew.borish@gmail.com>
2018-07-15 11:53:36 -0700

    updated to python 3

--- my_apod_v1_170211.py
+++ my_apod_v1_170211.py
@@ -1,10 +1,13 @@
 import os
 from datetime import datetime
 from datetime import timedelta
-import urllib
+import urllib3
 import sys
-from BeautifulSoup import *
+from bs4 import BeautifulSoup as bs
 import random
+import shutil
+import ctypes
+
 
 x = datetime.now()
 
@@ -12,44 +15,64 @@ myRI= random.randint(-1000,-1)
 
 y = timedelta(weeks=0, days=myRI, hours=0, minutes=0, seconds=0)
 
-print x
+print (x)
 
 z = x + y
 
 myDate = z.strftime('%y%m%d')
 
-print myDate
-
+print (myDate)
+http = urllib3.PoolManager()
 url = "https://apod.nasa.gov/apod/ap"+ myDate + ".html"
 
-html = urllib.urlopen(url).read()
-soup = BeautifulSoup(html)
+response = http.request('GET', url)
+soup = bs(response.data)
 
 hrefList = []
 # Retrieve all of the anchor tags
 tags = soup('a')
 for tag in tags:
-    print tag.get('href', None)
+    print (tag.get('href', None))
     hrefList.append(tag.get('href', None))
 
-print "\n"
+print ("\n")
 #print hrefList[1]
 
-print "\n"
+print ("\n")
 myImage = hrefList[1]
-print myImage
+print (myImage)
 
 
-urllib.urlretrieve("https://apod.nasa.gov/apod/" + myImage, "C:\\Wallpaper\\myApod.jpg")
+#urllib3.urlretrieve("https://apod.nasa.gov/apod/" + myImage, "C:\\Wallpaper\\myApod.jpg")
 
-import ctypes
-import os
+http.request('GET', "https://apod.nasa.gov/apod/" + myImage, preload_content=False)
+
+url = "https://apod.nasa.gov/apod/" + myImage
+c = urllib3.PoolManager()
+filename = "C:\\Wallpaper\\myApod.jpg"
+
+with c.request('GET', url, preload_content=False) as resp, open(filename, 'wb') as out_file:
+    shutil.copyfileobj(resp, out_file)
+
+resp.release_conn()     # not 100% sure this is required though
+
+
+
 drive = "C:\\"
 folder = "Wallpaper"
 image = "myApod.jpg"
 image_path = os.path.join(drive, folder, image)
-SPI_SETDESKWALLPAPER = 20
-ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, image_path, 3)
+
+
+SPI_SETDESKWALLPAPER  = 0x0014
+SPIF_UPDATEINIFILE    = 0x0001
+SPIF_SENDWININICHANGE = 0x0002
+
+user32 = ctypes.WinDLL('user32')
+SystemParametersInfo = user32.SystemParametersInfoW
+SystemParametersInfo.argtypes = ctypes.c_uint, ctypes.c_uint, ctypes.c_void_p, ctypes.c_uint
+#SystemParametersInfo.restype = wintypes.BOOL
+print(SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, image_path, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE))
 
 sys.exit()
 
