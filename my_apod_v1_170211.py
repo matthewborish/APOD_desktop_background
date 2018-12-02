29869e923012f5cecb0e54addf03da6f34df937d
matthew.borish@gmail.com <matthew.borish@gmail.com>
2018-12-02 12:49:48 -0800

    cleaned some stuff up

--- my_apod_v1_170211.py
+++ my_apod_v1_170211.py
@@ -10,69 +10,49 @@ import shutil
 import ctypes
 
 
-
 def get_random_date():
-
-    x = datetime.now()
-    myRI= random.randint(-1000,-1)
-    y = timedelta(weeks=0, days=myRI, hours=0, minutes=0, seconds=0)
-
-
-    z = x + y
-
-
-    random_date = z.strftime('%y%m%d')
+    now = datetime.now()
+    random_int = random.randint(-1000, -1)
+    td = timedelta(weeks=0, days=random_int, hours=0, minutes=0, seconds=0)
+    random_date = (now + td).strftime('%y%m%d')
     return random_date
-
-
 
 
 def scrape_apod(date):
     http = urllib3.PoolManager()
-    url = "https://apod.nasa.gov/apod/ap"+ date + ".html"
+    url = "https://apod.nasa.gov/apod/ap" + date + ".html"
 
     response = http.request('GET', url)
     soup = bs(response.data)
 
-    hrefList = []
+    href_list = []
     # Retrieve all of the anchor tags
     tags = soup('a')
     for tag in tags:
         print (tag.get('href', None))
-        hrefList.append(tag.get('href', None))
+        href_list.append(tag.get('href', None))
 
-    print ("\n")
-    #print hrefList[1]
+    my_image = href_list[1]
 
-    print ("\n")
-    myImage = hrefList[1]
-    print (myImage)
+    http.request('GET', "https://apod.nasa.gov/apod/" + my_image, preload_content=False)
 
-
-    #urllib3.urlretrieve("https://apod.nasa.gov/apod/" + myImage, "C:\\Wallpaper\\myApod.jpg")
-
-    http.request('GET', "https://apod.nasa.gov/apod/" + myImage, preload_content=False)
-
-    url = "https://apod.nasa.gov/apod/" + myImage
+    url = "https://apod.nasa.gov/apod/" + my_image
     c = urllib3.PoolManager()
     filename = os.path.join("C:\\", "Wallpaper", "myApod.jpg")
 
     with c.request('GET', url, preload_content=False) as resp, open(filename, 'wb') as out_file:
         shutil.copyfileobj(resp, out_file)
 
-    resp.release_conn()     # not 100% sure this is required though
+    resp.release_conn()
 
-
-    try:
+    if not os.path.exists(os.path.join("C:\\", "Wallpaper")):
         os.makedir(os.path.join("C:\\", "Wallpaper"))
-    except:
-        pass
 
     drive = "C:\\"
     folder = "Wallpaper"
     image = "myApod.jpg"
     image_path = os.path.join(drive, folder, image)
-    return(image_path)
+    return image_path
 
 
 def update_wallpaper(image_path):
@@ -83,14 +63,11 @@ def update_wallpaper(image_path):
     user32 = ctypes.WinDLL('user32')
     SystemParametersInfo = user32.SystemParametersInfoW
     SystemParametersInfo.argtypes = ctypes.c_uint, ctypes.c_uint, ctypes.c_void_p, ctypes.c_uint
-    #SystemParametersInfo.restype = wintypes.BOOL
     print(SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, image_path, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE))
 
 
 x = get_random_date()
 update_wallpaper(scrape_apod(date=x))
-
-
 
 sys.exit()
 
